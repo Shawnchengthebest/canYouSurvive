bc1dab305ccc295db49064e3faf0535eec1deeeb
shawn <141798144+Shawnchengthebest@users.noreply.github.com>
2023-11-20 22:49:01 -0700

    enemy follow

--- canYouSurvive/Assets/Scripts/Enemy.cs
+++ canYouSurvive/Assets/Scripts/Enemy.cs
@@ -4,9 +4,26 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
+    public GameObject Player;
     public int health = 100;
     public GameObject deathEffect;
+    public float speed;
+    public bool flip;
     // Start is called before the first frame update
+    private void Update()
+    {
+        Vector3 scale = transform.localScale;
+        if (Player.transform.position.x > transform.position.x)
+        {
+            scale.x = Mathf.Abs(scale.x) * -1 * (flip ? -1 : 1);
+            transform.Translate(speed * Time.deltaTime, 0, 0);
+        } else
+        {
+            scale.x = Mathf.Abs(scale.x) * (flip ? -1 : 1);
+            transform.Translate(speed * Time.deltaTime * -1, 0, 0);
+        }
+
+    }
     public void TakeDamage (int damage)
     {
         health -= damage;
